caa58aa456b073853f60c26a703b586331f34559
jasonstorm <json329731049@gmail.com>
2017-10-20 21:46:10 +0800

    第一次commit，把自己平时的笔记记录起来

---
+++ JAVA笔记/Hello.java
@@ -0,0 +1,11 @@
+public class changeString {
+ 
+    public static void changeStr(String str){
+        str="welcome";
+    }
+    public static void main(String[] args) { 
+        String str="1234";
+        changeStr(str);
+        System.out.println(str);
+    }
+}
